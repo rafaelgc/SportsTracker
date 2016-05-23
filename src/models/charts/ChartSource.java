8c920f2ce81268a82ee7bcba856a556db5e1a71e
Rafa <rafag.1858@hotmail.es>
2016-05-23 23:27:56 +0200

    Añadida la optimización que había versiones atrás que consiste en la eliminación de un porcentaje de los puntos de la gráfica para que se dibuje más rápidamente. En este caso, el porcentaje es fijo y es del 80%.

--- src/models/charts/ChartSource.java
+++ src/models/charts/ChartSource.java
@@ -15,22 +15,54 @@ import jgpx.model.analysis.Chunk;
  * @author Rafa
  */
 public abstract class ChartSource {
-    
+
     private String name;
     private ObservableList<XYChart.Data<Number, Number>> data;
-    
+
+    //OPTIMIZACIÓN
+    private double optimizationFactor;
+    private int l;
+    private int counter;
+    double maxVAxis, maxAbscissa;
+    //FIN OPTIMIZACIÓN
+
     public ChartSource(String name) {
         this.name = name;
         data = FXCollections.observableArrayList();
+
+        //OPTIMIZACIÓN
+        this.optimizationFactor = 0.2d;
+        l = (int) (1 / optimizationFactor);
+        counter = 0;
+        maxVAxis = 0.d;
+        maxAbscissa = 0.d;
     }
-    
+
+    public boolean canAdd(Chunk c) {
+        boolean ret = false;
+        
+        if (getAbscissaValue(c) > maxVAxis) {
+            maxVAxis = getAbscissaValue(c);
+            ret = true;
+        }
+
+        if (counter == l) {
+            counter = 0;
+            ret = true;
+        }
+        
+        counter++;
+        
+        return ret;
+    }
+
     public String getName() {
         return name;
     }
-    
+
     public ObservableList<XYChart.Data<Number, Number>> getData() {
         return data;
     }
-    
+
     public abstract double getAbscissaValue(Chunk chunk);
 }
--- src/models/charts/ChartTask.java
+++ src/models/charts/ChartTask.java
@@ -8,11 +8,7 @@ package models.charts;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import jgpx.model.analysis.Chunk;
 import jgpx.model.analysis.TrackData;
@@ -22,22 +18,22 @@ import jgpx.model.analysis.TrackData;
  * @author Rafa
  */
 public abstract class ChartTask extends Task<XYChart<Number, Number>> {
-
+    
     private TrackData trackData;
-
+    
     private List<ChartSource> chartSources;
     private Abscissa abscissa;
     private String xName, yName;
     private boolean legendVisible;
-
+    
     public enum Abscissa {
         DISTANCE, TIME
     };
-
+    
     public ChartTask(TrackData td, Abscissa abscissa) {
         trackData = td;
         chartSources = new ArrayList<>();
-
+        
         this.abscissa = abscissa;
         if (abscissa == Abscissa.DISTANCE) {
             xName = "Distancia (km)";
@@ -54,23 +50,23 @@ public abstract class ChartTask extends Task<XYChart<Number, Number>> {
     public void setYName(String yName) {
         this.yName = yName;
     }
-
+    
     public void addChartSource(ChartSource source) {
         chartSources.add(source);
     }
-
+    
     @Override
     protected XYChart<Number, Number> call() throws Exception {
         return createGraph(trackData);
     }
-
+    
     private XYChart<Number, Number> createGraph(TrackData td) {
         long ini = System.nanoTime();
         long totalIni = ini;
         List<Chunk> chunks = td.getChunks();
-
+        
         XYChart<Number, Number> chart = chartInstance();
-
+        
         chart.setPrefHeight(350);
         chart.getXAxis().setLabel(xName);
         chart.getYAxis().setLabel(yName);
@@ -83,34 +79,36 @@ public abstract class ChartTask extends Task<XYChart<Number, Number>> {
             series.setName(chartSources.get(i).getName());
             chart.getData().add(series);
         }
-
+        
         fillData(chunks);
-
+        
         return chart;
     }
-
+    
     protected void fillData(List<Chunk> chunks) {
         double abscissaValue = 0.d;
-
+        
         for (ChartSource c : chartSources) {
             c.getData().clear();
         }
-
+        
         for (Iterator<Chunk> it = chunks.iterator(); it.hasNext();) {
             Chunk c = it.next();
             
             for (ChartSource source : chartSources) {
-                source.getData().add(new XYChart.Data<>(abscissaValue, source.getAbscissaValue(c)));
+                if (source.canAdd(c)) {
+                    source.getData().add(new XYChart.Data<>(abscissaValue, source.getAbscissaValue(c)));
+                }
             }
-
+            
             if (abscissa == Abscissa.TIME) {
                 abscissaValue += (c.getDuration().getSeconds()) / 60.d;
             } else {
                 abscissaValue += c.getDistance() / 1000.d;
             }
-
+            
         }
     }
-
+    
     abstract XYChart<Number, Number> chartInstance();
 }
